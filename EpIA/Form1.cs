7e0ca8fc40a70f6c8b339bc736ad43f1afbb61c6
Lucas Picazio <Administrador@LAB07M16P017222.uspeachsi.uspnet>
2019-04-10 21:56:13 -0300

    ta indo

--- EpIA/Form1.cs
+++ EpIA/Form1.cs
@@ -12,27 +12,81 @@ namespace EpIA
 {
 	public partial class Form1 : Form
 	{
+
+        DataTable dt = new DataTable();
 		int[,] popini  = new int[10,20]{
-				{0,0,0,0,0,1,0,1,1,0,0,0,1,1,1,0,1,1,1,0},				{1,0,0,0,0,0,1,1,0,0,1,0,1,0,1,1,1,1,1,0},				{0,1,0,1,0,0,0,0,0,1,0,1,0,1,1,0,1,1,1,1},				{0,1,1,1,1,1,0,0,0,1,0,1,1,1,0,0,0,1,0,0},				{1,0,1,1,1,1,0,0,0,0,0,1,1,1,0,1,0,1,0,1},				{0,1,0,0,0,1,0,0,1,0,1,0,1,0,0,1,0,1,1,1},				{1,1,0,1,1,0,1,0,1,1,0,0,0,0,0,1,0,1,1,1},				{0,1,0,1,1,0,0,0,0,0,1,1,0,1,1,0,0,0,0,0},				{1,0,0,1,1,1,0,0,1,0,0,1,1,0,1,1,1,0,1,1},				{0,0,1,1,0,0,1,1,1,0,1,0,1,0,1,1,0,0,1,1}
-		 };		double x;		double y;
-		public Form1()
+				{0,0,0,0,0,1,0,1,1,0,0,0,1,1,1,0,1,1,1,0},
+				{1,0,0,0,0,0,1,1,0,0,1,0,1,0,1,1,1,1,1,0},
+				{0,1,0,1,0,0,0,0,0,1,0,1,0,1,1,0,1,1,1,1},
+				{0,1,1,1,1,1,0,0,0,1,0,1,1,1,0,0,0,1,0,0},
+				{1,0,1,1,1,1,0,0,0,0,0,1,1,1,0,1,0,1,0,1},
+				{0,1,0,0,0,1,0,0,1,0,1,0,1,0,0,1,0,1,1,1},
+				{1,1,0,1,1,0,1,0,1,1,0,0,0,0,0,1,0,1,1,1},
+				{0,1,0,1,1,0,0,0,0,0,1,1,0,1,1,0,0,0,0,0},
+				{1,0,0,1,1,1,0,0,1,0,0,1,1,0,1,1,1,0,1,1},
+				{0,0,1,1,0,0,1,1,1,0,1,0,1,0,1,1,0,0,1,1}
+		 };
+		double x;
+		double y;
+
+        double fitness;
+
+        string lineString;
+
+        DataRow dr;
+
+
+        public Form1()
 		{
 			InitializeComponent();
-		}
+            DataColumn binx = new DataColumn("binx");
+            DataColumn biny = new DataColumn("biny");
+            DataColumn decx = new DataColumn("decx");
+            decx.DataType = typeof(double);
+            DataColumn decy = new DataColumn("decy");
+            decy.DataType = typeof(double);
+            DataColumn resultx = new DataColumn("resultx");
+            DataColumn resulty = new DataColumn("resulty");
+            DataColumn fitnessx = new DataColumn("fitnessx");
+            DataColumn fitnessy = new DataColumn("fitnessy");
+            DataColumn sum = new DataColumn("sum");
+            dt.Columns.Add(binx);
+            dt.Columns.Add(biny);
+            dt.Columns.Add(decx);
+            dt.Columns.Add(decy);
+            dt.Columns.Add(resultx);
+            dt.Columns.Add(resulty);
+            dt.Columns.Add(fitnessx);
+            dt.Columns.Add(fitnessy);
+            dt.Columns.Add(sum);
 
-		private void Button1_Click(object sender, EventArgs e)
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
 		{
-			bintodec();
-		}
+            
+            Rodar();
+           Selecao();
 
-		private void bintodec()
+        }
+
+        private void Selecao()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void bintodec(string line)
 		{
 			
-			var line = popini.Cast<int>().Take(20).ToArray();
-			string lineString = string.Join("",line);
-			string xString = lineString.Substring(0,10);
-			string yString = lineString.Substring(10, 10);
-			x = Convert.ToInt32(xString,2);
+			
+			
+            
+			string xString = line.Substring(0,10);
+			string yString = line.Substring(10, 10);
+            dr["binx"] = xString;
+            dr["biny"] = yString;
+            x = Convert.ToInt32(xString,2);
 			y = Convert.ToInt32(yString, 2);
 
 			x = (x * 0.00978) -5;
@@ -41,6 +95,32 @@ namespace EpIA
 			x = Math.Round(x, 2);
 			y = Math.Round(y, 2);
 
-		}
-	}
+            dr["decx"] = x;
+            dr["decy"] = y;
+
+
+
+        }
+
+        private void Rodar()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                
+                var line = popini.Cast<int>().Skip(i * 20).Take(20).ToArray(); // pega cada linha da matriz
+                string lineString = string.Join("",line); // array to string
+
+                dr = dt.NewRow();
+                
+             
+                bintodec(lineString);
+                
+                Rodar();
+                Console.WriteLine("Fitnes {0}: {1}", i + 1, fitness);
+
+            var result1 = 20 + Math.Pow(x, 2) + Math.Pow(y, 2) - 10 * (Math.Cos(2 * Math.PI * x) + Math.Cos(2 * Math.PI * y));
+            fitness = 1 / result1;
+            }
+        }
+    }
 }
